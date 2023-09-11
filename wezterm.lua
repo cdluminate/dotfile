51dcabf147cfa3fb67976152962eef8ab12f02d8
Mo Zhou <cdluminate@gmail.com>
2023-09-10 20:44:04 -0400

    dotfile: add wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,10 @@
+-- copy to ~/.wezterm.lua
+local wezterm = require 'wezterm'
+
+return {
+    font = wezterm.font 'Operator Mono',
+--    color_scheme = "Atom",
+    color_scheme = "Dark Pastel",
+--    color_scheme = "Dark+",
+    font_size = 23,
+}
